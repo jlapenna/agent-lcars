9aac3a57e90c2bfaa00686cdb53ec0ab4252b709
Test <test@test.com>
2026-08-14 05:37:08 -0700

    feat(control-plane): bind exact runs atomically

--- libs/lifecycle-control-plane/src/index.ts
+++ libs/lifecycle-control-plane/src/index.ts
@@ -1,4 +1,5 @@
 export * from './attempt-reducer';
 export * from './authority-storage';
 export * from './ingress-policy';
+export * from './launch-binding';
 export * from './task-intent-reducer';
